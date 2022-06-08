5e8f259a3322ab35187a181184d2979e843ac638
imamura mitsuo <minhyeong.toshita@gmail.com>
2022-06-08 22:33:07 +0900

    本当にこんな愚直な書き方で良いのか・・・

--- src/index.js
+++ src/index.js
@@ -1,17 +1,58 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-class Test extends React.Component {
+class Personal extends React.Component {
   render() {
-    return (
-      <div>
-        <h1>h1!</h1>
-      </div>
-    );
+    return <div>パーソナルデータ</div>;
+  }
+}
+
+class Ability extends React.Component {
+  render() {
+    return <div>能力値・判定値</div>;
+  }
+}
+
+class Talent extends React.Component {
+  render() {
+    return <div>特性・タレント</div>;
+  }
+}
+
+class Equipment extends React.Component {
+  render() {
+    return <div>武器防具装飾品・強化</div>;
+  }
+}
+
+class Trade extends React.Component {
+  render() {
+    return <div>購入・売却履歴</div>;
+  }
+}
+
+class Growth extends React.Component {
+  render() {
+    return <div>成長履歴</div>;
   }
 }
 
 // ========================================
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Test />);
+const personal = ReactDOM.createRoot(document.getElementById("personal"));
+personal.render(<Personal />);
+
+const ability = ReactDOM.createRoot(document.getElementById("ability"));
+ability.render(<Ability />);
+
+const talent = ReactDOM.createRoot(document.getElementById("talent"));
+talent.render(<Talent />);
+
+const equipment = ReactDOM.createRoot(document.getElementById("equipment"));
+equipment.render(<Equipment />);
+
+const trade = ReactDOM.createRoot(document.getElementById("trade"));
+trade.render(<Trade />);
+
+const growth = ReactDOM.createRoot(document.getElementById("growth"));
+growth.render(<Growth />);
